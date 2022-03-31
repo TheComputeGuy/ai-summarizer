de2b2cb42c6c7b13c136b362f7c07b7b66941829
Shubham <shubhampharande8@gmail.com>
2022-04-01 01:32:03 +0530

    Added PDF upload endpoint and basic upload handling

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, abort
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
+UPLOAD_FOLDER = './temp/uploads'
+
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1000 * 1000
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
 
 @app.route('/summary', methods=['POST'])
@@ -19,6 +25,37 @@ def get_summary():
     output_text["message"] = "Request complete"
 
     return output_text
+
+@app.route('/pdfSummary', methods=['POST'])
+def get_pdf_summary():
+
+     # check if the post request has the file part
+    if 'input_file' not in request.files:
+        output = {}
+        output["status"] = "400"
+        output["message"] = "File missing in request"
+        return output
+
+    file = request.files['input_file']
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Either save the file as ^ or directly feed it into the PDF parser
+        # Get output text. Then pass it to the model method
+
+        input_text = "dummy"
+        summary_text = "dummy"
+
+        output = {}
+        output["input"] = input_text
+        output["summary"] = summary_text
+        output["status"] = "200"
+        output["message"] = "Request complete"
+
+        return output
+
+    return abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
